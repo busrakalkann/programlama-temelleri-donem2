44c8c4285424955cfca0bb73b9195cce33250920
busrakalkann <busrakalkn0@gmail.com>
2025-04-07 12:08:44 +0300

    Add files via upload

---
+++ 250407/ornek1.py
@@ -0,0 +1,7 @@
+derece=int(input("sayi gir"))
+if derece <0:
+    print("su katıdır")
+elif derece >=0 and derece <100:
+    print("su sıvıdır")
+else:
+    print("su buhardır")
---
+++ 250407/ornek2.py
@@ -0,0 +1,7 @@
+sayi=int(input("sayı gir"))
+if sayi>0:
+    print("sayı pozıtıftır")
+elif sayi <0:
+    print("sayı negatif")
+else:
+    print("sayı nötr")
